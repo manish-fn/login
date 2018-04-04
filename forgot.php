68a07565cb5386190841e269c6a87dc32d61b37f
Manish <manish@fixnix.co>
2018-04-04 13:24:37 +0530

    Login

---
+++ forgot.php
@@ -0,0 +1,15 @@
+<?php
+require 'database.php';
+$a=$_POST['email'];
+?>					
+<!DOCTYPE html>
+<html>
+<head>
+<title>forgot password</title>
+</head>
+<body>
+<input type="text" name="email" placeholder="Enter the e-mail id"/>
+<input type="submit" name="submit" value="forgot"/>
+</body>
+</html>
+
