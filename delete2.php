be146a1cfbd2b04c8aac80b5a5e869c12440d2d3
Manish <manish@fixnix.co>
2018-04-18 17:21:06 +0530

    Edit del2

--- delete2.php
+++ delete2.php
@@ -7,10 +7,10 @@ $b=$_POST['password'];
 $sql = "DELETE FROM student WHERE email = '$a' AND password ='$b'";
 mysqli_query($conn, $sql);
 if (mysqli_affected_rows($conn) > 0) {
-    echo "You have successfully updated your data.<br><br>";
+    echo "User deleted<br><br>";
 }
 else {
-    echo "The data you submitted matched the current data so nothing was changed.<br><br>";
+    echo "Nothing was changed.<br><br>";
 }
 mysqli_close($conn);
 ?>
